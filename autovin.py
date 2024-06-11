412e95dfa0ca16af27af58fc87641c2be823043e
ChanMichelin <chanhyung.kim@nextraq.com>
2024-06-11 15:07:24 -0400

    Update autovin.py

--- autovin.py
+++ autovin.py
@@ -169,7 +169,7 @@ def add_header():
     st.markdown(
         """
         <header>
-            <img src="assets/michelin-logo.png">
+            <img src="assets/michelin-logo.png" alt="Michelin">
             <h1>Michelin Connected Fleet</h1>
         </header>
         """,
